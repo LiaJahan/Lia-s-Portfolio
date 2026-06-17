d0e8e6f503a8983133c28e8e6a0d625684df2411
LiaJahan <jesmoonlia@gmail.com>
2026-06-17 19:17:17 +0600

    adding basename

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,15 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import ProjectDetails from "../pages/ProjectDetails";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/project/:id",
+      element: <ProjectDetails />,
+    },
+  ],
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/project/:id",
-    element: <ProjectDetails />,
-  },
-]);
+    basename: "/Lia-s-Portfolio",
+  }
+);
 
 export default router;
